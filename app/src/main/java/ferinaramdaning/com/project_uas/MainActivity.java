2d96c1dda440507188389a434e0f02bd6ad7ecf2
ferinabay <ferinabayu@gmail.com>
2019-05-23 08:58:43 +0700

    create project

---
+++ app/src/main/java/ferinaramdaning/com/project_uas/MainActivity.java
@@ -0,0 +1,13 @@
+package ferinaramdaning.com.project_uas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
